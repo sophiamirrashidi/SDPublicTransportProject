5d1edfebf76a86804d968ed40db90418af6dd465
Sophia Mirrashidi <smirrashidi@ucsb.edu>
2023-03-30 21:44:53 -0700

    cleaning up

---
+++ scripts/block.py
@@ -0,0 +1,23 @@
+class block:
+    def __init__(self, blockid, income, pop)
+        self.blockid = blockid
+        self.income = income
+        self.pop = pop
+
+    def setBlockid(self, blockid):
+        self.blockid = blockid
+
+    def setIncome(self, income):
+        self.income = income
+
+    def setPop(self, pop):
+        self.pop = pop
+
+    def getBlockid(self):
+        return self.blockid
+
+    def getIncome(self):
+        return self.income
+
+    def getPop(self):
+        return self.pop
--- scripts/stop.py
+++ scripts/stop.py
@@ -36,27 +36,3 @@ class stop:
     def getPop(self):
         return self.pop
     
-
-class block:
-    def __init__(self, blockid, income, pop)
-        self.blockid = blockid
-        self.income = income
-        self.pop = pop
-
-    def setBlockid(self, blockid):
-        self.blockid = blockid
-
-    def setIncome(self, income):
-        self.income = income
-
-    def setPop(self, pop):
-        self.pop = pop
-
-    def getBlockid(self):
-        return self.blockid
-
-    def getIncome(self):
-        return self.income
-
-    def getPop(self):
-        return self.pop
